7affa5ee50b8e3d3145d96c4e9d0966c3d251f16
Shamir Gibbons <shamir.gibbons@gmail.com>
2020-04-28 21:10:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 function dwarfRollCall(dwarves) {
+	var endString = ""
 }
-
+  for (var i = 0; i < dwarves.length; i++) {
+   endString += (i+1).toString()+ ". " + dwarves[i] + " ";
+  }
+  return endString;
+  
+  
+  
+  
+  
+  
 function summonCaptainPlanet(planeteerCalls){
+	var calls = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+   calls.push(planeteerCalls[i] + "!");
+  }
+  return calls;
 }
 
 function longPlaneteerCalls(words) {
+	//your code here!
+	var hasLongWords = false;
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4){
+      hasLongWords = true;
+    }
+  }
+  return hasLongWords;
 }
 
+
+
+
 function findTheCheese (foods) {
+	var cheeses = ["cheddar", "gouda", "camembert"];
+	for (var i = 0; i < foods.length; i++) {
+		for (var j = 0; j < cheeses.length; j++) {
+			if ( cheeses[j]=== foods[i] ) {
+				return foods[i]  that;
+			}
+		}
+	}
+	return "no cheese!";
 }
+   
